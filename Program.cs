97cfd62fdff0193129cc9b59bab575175538807e
Wanderson Alves Rodrigues <wanderson.alves.rodrigues@gmail.com>
2024-08-20 08:47:30 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing Opa", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing One", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
