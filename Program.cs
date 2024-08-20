e9d086541b952397d45fb91e6b86a4baa38d6c3b
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2024-08-19 23:35:11 -0300

    feat: alteração API

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing One", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching Two"
 };
 
 app.MapGet("/weatherforecast", () =>
